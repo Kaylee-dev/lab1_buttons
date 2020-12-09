285eabbe60d8f2ba8d770d750080383b2d73ec69
Kaylee <99057337@mydavinci.nl>
2020-12-09 11:31:43 +0100

    Buttons 2.0 gemaakt

--- buttons.js
+++ buttons.js
@@ -4,31 +4,48 @@ var backgroundImage = document.getElementById("container")
 var button1 = document.getElementById("button1");
 var count1 = 0;
 
-button1.onclick = function() {
-	imageCorner.src = "images/1.jpg";
-	backgroundImage.style.backgroundImage = "url('images/bg1.jpg')";
-	count1 += 1;
-	button1.innerHTML = count1;
-}
-
-
 var button2 = document.getElementById("button2");
 var count2 = 0;
-
-button2.onclick = function() {
-	imageCorner.src = "images/2.jpg";
-	backgroundImage.style.backgroundImage = "url('images/bg2.jpg')";	
-	count2 += 1;
-	button2.innerHTML = count2;
-}
-
 
 var button3 = document.getElementById("button3");
 var count3 = 0;
 
+button1.onclick = function() {
+	if (button1.style.backgroundColor != "red") {
+		button1.style.backgroundColor = "#66ff99";
+		button2.style.backgroundColor = "#66ff99";
+		button3.style.backgroundColor = "#66ff99";
+		imageCorner.src = "images/1.jpg";
+		backgroundImage.style.backgroundImage = "url('images/bg1.jpg')";
+		button1.style.backgroundColor = "red";
+		count1 += 1;
+		button1.innerHTML = count1;
+	}
+}
+
+button2.onclick = function() {
+	if (button2.style.backgroundColor != "red") {
+		button1.style.backgroundColor = "#66ff99";
+		button2.style.backgroundColor = "#66ff99";
+		button3.style.backgroundColor = "#66ff99";
+		imageCorner.src = "images/2.jpg";
+		backgroundImage.style.backgroundImage = "url('images/bg2.jpg')";	
+		button2.style.backgroundColor = "red";
+		count2 += 1;
+		button2.innerHTML = count2;
+	}
+}
+
+
 button3.onclick = function() {
-	imageCorner.src = "images/3.jpg";
-	backgroundImage.style.backgroundImage = "url('images/bg3.jpg')";
- 	count3 += 1;
- 	button3.innerHTML = count3;
+	if (button3.style.backgroundColor != "red") {
+		button1.style.backgroundColor = "#66ff99";
+		button2.style.backgroundColor = "#66ff99";
+		button3.style.backgroundColor = "#66ff99";
+		imageCorner.src = "images/3.jpg";
+		backgroundImage.style.backgroundImage = "url('images/bg3.jpg')";
+		button3.style.backgroundColor = "red";
+	 	count3 += 1;
+	 	button3.innerHTML = count3;
+	}
 }
